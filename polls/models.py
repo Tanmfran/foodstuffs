6b674323fcdc8e039df9c25349b962a659acad59
tannorfranks@gmail.com <tannor1@yahoo.com>
2020-02-14 11:29:11 -0600

    first commit

---
+++ polls/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+# Create your models here.
+class FoodItems(models.Model):
+    item_name = models.CharField(max_length=200)
+    item_cost = models.DecimalField(default=0.00, decimal_places=2, max_digits=6)
+    item_url = models.CharField(max_length=512)
